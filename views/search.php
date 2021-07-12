9302876dae6fc0732a58a73c23cda69e9d1541ba
yapiko5505 <bloomingjewels7021@gmail.com>
2021-07-13 08:36:31 +0900

    clone1

---
+++ views/search.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    
+    <?php include_once('../views/common/head.php'); ?>
+    <title>検索画面/twitterクローン</title>
+    <meta name="discription" content="検索画面です">
+
+</head>
+<body class="home">
+    <div class="container">
+        <?php include_once('../views/common/side.php'); ?>
+        <div class="main">
+            <div class="main-header">
+                <h1>検索</h1>
+                
+            </div>
+
+            <form action="search.php" method="get">
+                <div class="search-area">
+                    <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="<?php echo htmlspecialchars($view_keyword); ?>">
+                    <button type="submit" class="btn">検索</button>
+                </div>
+            </form>
+
+            <div class="ditch"></div>
+
+            <?php if (empty($view_tweets)): ?>
+                <p class="p-3">ツイートがまだありません</p>
+            <?php else: ?>
+                <div class="tweet-list">
+                    <?php foreach ($view_tweets as $view_tweet): ?>
+                        <?php include('../views/common/tweet.php'); ?>
+                    <?php endforeach; ?>
+
+                </div>
+            <?php endif;?>
+        </div>
+    </div>
+
+    <?php include_once('../views/common/foot.php'); ?>
+</body>
+
+</html>
